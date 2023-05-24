7a6d80eba00547bd7fc47d676be43b86f6327e69
Bob McAleavey <bobmcaleavey@gmail.com>
2023-05-24 12:08:37 -0400

    commit

---
+++ src/routes/(api)/api/email/+server.ts
@@ -0,0 +1,47 @@
+import { promises as fs } from 'fs';
+import inlineCss from 'inline-css';
+import handlebars from 'handlebars';
+import nodemailer from 'nodemailer';
+import { NODEMAILER_PASSWORD } from '$env/static/private';
+
+const getTemplate = async (template: string) => {
+	const html = await fs.readFile(
+		`./src/routes/(email-templates)/email-templates/${template}/html.html`,
+		{ encoding: 'utf-8' }
+	);
+	return html;
+};
+
+const handlebarsReplacements = (html: string, replacements: { [key: string]: any }) => {
+	const template = handlebars.compile(html);
+	const replacedHtml = template(replacements);
+	return replacedHtml;
+};
+
+export const POST = async ({ request }) => {
+	const { from, replacements, subject, template, to } = await request.json();
+
+	// setup transporter
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: 'abtl.noreply@gmail.com',
+			pass: NODEMAILER_PASSWORD
+		}
+	});
+
+	// get html from template
+	const html = await inlineCss(handlebarsReplacements(await getTemplate(template), replacements));
+
+	// setup options
+	const options = {
+		html,
+		from,
+		to,
+		subject
+	};
+
+	// send email
+	await transporter.sendMail(options);
+	return new Response();
+};
